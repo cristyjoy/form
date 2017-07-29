d59fb5622266cc564ecff19c792b3dc01696feb4
cristyjoy <cristangengs.04@gmail.com>
2017-07-29 09:22:52 +0800

    views and urls

--- student/admin.py
+++ student/admin.py
@@ -3,22 +3,36 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Detail, Course, Subject 
+from .models import Detail, Course, Subject
 
 # Register your models here.
 
-#@admin.register(Details)
-class StudentAdmin(admin.ModelAdmin):
-	fieldset = [
-		("Student Details", {"fields":["first_name","last_name"]})
-	]
-	def course_detail(self, obj):
-		return obj.list_detail()
 
-list_display = ("first_name","last_name",)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+	list_display = ("course_name","description",)
+	list_editable = ("description",)
+	list_filter = ("course_name",)
+	search_fields = ("course_name", "description",)
 
-admin.site.register(Detail)
-admin.site.register(Course)
-admin.site.register(Subject)
+@admin.register(Detail)
+class DetailAdmin(admin.ModelAdmin):
+	list_display = ("last_name",)
+	list_editable = ("last_name",)
+	list_filter = ("last_name",)
+	search_fields = ("last_name",)
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+	list_display = ("subject_name", "description",)
+	list_editable = ("description",)
+	list_filter = ("subject_name",)
+	search_fields = ("subject_name",)
+		
+
+
+#admin.site.register(Detail)
+#admin.site.register(Course, CourseAdmin)
+#admin.site.register(Subject)
+
 
 
